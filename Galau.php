239e9795806f40f1001f2abadea91742731aeb1e
Daffa Takarai <40546955+DaffaTakarai@users.noreply.github.com>
2018-11-28 00:12:21 +0700

    Add files via upload

---
+++ Galau.php
@@ -0,0 +1,12 @@
+<?php
+//Daffa Takarai
+//ByteDevC0de
+ $cinta_ku  = "99%";
+ $cinta_mu  = "1%";
+if ($cinta_ku > $cinta_mu) {
+	   echo "Mencari Nama Mu Hal Tersulit Bagi Ku:v";
+} else {
+	   echo "Masa Sih Padahal Tinggal Cari :v";
+}
+
+?>
